887188fe558265c921d78ee8f24991d93fbc6948
油炸小鱼干 <3140864625@qq.com>
2023-12-19 21:13:34 +0800

    '人麻了'

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ function App() {
                   <span className="Col1_Span2">0</span>
                   <span>/年</span>
                   <div>
-                      <div className="Col1_Span3">5人成员上限</div>
-                      <div className="Col1_Span3">5G容量</div>
-                      <div className="Col1_Span3">单文件最大100M</div>
-                      <div className="Col1_Span3">单项目最大1G</div>
-                      <div className="Col1_Span3">社区与邮件支持</div>
+                      <div className="Spacing">5人成员上限</div>
+                      <div className="Spacing">5G容量</div>
+                      <div className="Spacing">单文件最大100M</div>
+                      <div className="Spacing">单项目最大1G</div>
+                      <div className="Spacing">社区与邮件支持</div>
+                      <div className="Col1_dis">color:white_字体白色占了一行空格</div>
                       <div className="btn">免费试用</div>
                   </div>
               </div>
@@ -23,17 +24,20 @@ function App() {
           <div id="Col2">
               <div className="Col1_title">标准版</div>
               <div>
-                  <span className="Col1_Span1">￥</span>
-                  <span className="Col1_Span2">1998</span>
-                  <span>/年</span>
                   <div>
-                      <div className="Col1_Span3">20人成员上限</div>
-                      <div className="Col1_Span3">20G容量</div>
-                      <div className="Col1_Span3">单文件最大100M</div>
-                      <div className="Col1_Span3">单项目最大1G</div>
-                      <div className="Col1_Span3">社区与邮件支持在线与电话支持</div>
-                      <div className="Col1_Span3">短信通知</div>
-                      <div className="btn">购买</div>
+                      <span className="Col2_Span1">￥</span>
+                      <span className="Col2_Span2">1998</span>
+                      <span>/年</span>
+                  </div>
+
+                  <div>
+                      <div className="Spacing">20人成员上限</div>
+                      <div className="Spacing">20G容量</div>
+                      <div className="Spacing">单文件最大100M</div>
+                      <div className="Spacing">单项目最大1G</div>
+                      <div className="Spacing">社区与邮件支持在线与电话支持</div>
+                      <div className="Spacing">短信通知</div>
+                      <div className="btn2">购买</div>
                   </div>
               </div>
           </div>
@@ -44,12 +48,12 @@ function App() {
                   <span className="Col1_Span2">3998</span>
                   <span>/年</span>
                   <div>
-                      <div className="Col1_Span3">50人成员上限</div>
-                      <div className="Col1_Span3">50G容量</div>
-                      <div className="Col1_Span3">单文件最大200M</div>
-                      <div className="Col1_Span3">单项目最大2G</div>
-                      <div className="Col1_Span3">社区与邮件支持在线与电话支持</div>
-                      <div className="Col1_Span3">短信通知</div>
+                      <div className="Spacing">50人成员上限</div>
+                      <div className="Spacing">50G容量</div>
+                      <div className="Spacing">单文件最大200M</div>
+                      <div className="Spacing">单项目最大2G</div>
+                      <div className="Spacing">社区与邮件支持在线与电话支持</div>
+                      <div className="Spacing">短信通知</div>
                       <div className="btn">购买</div>
                   </div>
               </div>
